66564129914ce967a01d8bf0bcdee45dfebedcd1
xereleza <xereleza@github.com>
2022-12-20 13:51:06 -0300

    assignment 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    renderDish(dish) {
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    };
+
+    renderComments(comments) {
+        if (comments != null) 
+            return (
+                <div>
+                    <h4>Comments</h4>
+                    {comments.map((comment) => {
+                        return (
+                            <ul key={comment.id} className='list-unstyled'>
+                                <li>{comment.comment}</li>
+                                <li>-- {comment.author} , {comment.date}</li>
+                            </ul>
+
+                        );
+                    })}
+                </div>
+            );
+        else return <div></div>;
+    };
+
+    render() {
+        const dish = this.props.dish;
+        if (dish != null) 
+            return (
+                <div className='row'>
+                    <div className='col-12 col-md-5 m-1'>
+                        {this.renderDish(dish)}
+                    </div>
+                    <div className='col-12 col-md-5 m-1'>
+                        {this.renderComments(dish.comments)}
+                    </div>
+                </div>
+            );
+        else return <div></div>;
+    };
+};
+
+export default DishDetail;
