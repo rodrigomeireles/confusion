cd4875118eb3c10aaf1bdcacdc81c32fe4d7a3af
xereleza <xereleza@github.com>
2023-02-07 09:48:53 -0300

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -43,12 +43,14 @@ class DishDetail extends Component {
         const dish = this.props.dish;
         if (dish != null) 
             return (
-                <div className='row'>
-                    <div className='col-12 col-md-5 m-1'>
-                        {this.renderDish(dish)}
-                    </div>
-                    <div className='col-12 col-md-5 m-1'>
-                        {this.renderComments(dish.comments)}
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12 col-md-5 m-1'>
+                            {this.renderDish(dish)}
+                        </div>
+                        <div className='col-12 col-md-5 m-1'>
+                            {this.renderComments(dish.comments)}
+                        </div>
                     </div>
                 </div>
             );
