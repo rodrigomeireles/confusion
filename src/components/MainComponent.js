f1f91d46f57fc1a7d5ec116caf01ea333f3aee4c
xereleza <xereleza@github.com>
2023-02-08 20:08:23 -0300

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -22,8 +22,6 @@ class Main extends Component {
         };
     };
 
-    onDishSelect(dishId) { this.setState({ selectedDish: dishId }); };
-
     render() {
         const HomePage = () => {
             return (
@@ -33,12 +31,22 @@ class Main extends Component {
                 />
             );
         };
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
